f66d6cb51d9cd337dc8a8b459e7a5512e198d996
muskarab <mustafa.kamal.mk858@gmail.com>
2022-01-14 13:29:04 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,10 @@ use App\Http\Livewire\Auth\ForgotPassword;
 use App\Http\Livewire\Auth\ResetPassword;
 use App\Http\Livewire\Auth\SignUp;
 use App\Http\Livewire\Auth\Login;
-use App\Http\Livewire\Dashboard;
-use App\Http\Livewire\Billing;
-use App\Http\Livewire\Profile;
-use App\Http\Livewire\Tables;
-use App\Http\Livewire\StaticSignIn;
-use App\Http\Livewire\StaticSignUp;
-use App\Http\Livewire\Rtl;
-
-use App\Http\Livewire\LaravelExamples\UserProfile;
-use App\Http\Livewire\LaravelExamples\UserManagement;
 use App\Models\Article;
 use App\Models\Slider;
 use App\Models\Team;
 use App\Models\Testimoni;
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -96,13 +85,5 @@ Route::middleware('auth')->group(function () {
         Route::post('/testimonies/update', [TestimoniController::class, 'update'])->name('update_testimoni');
         Route::delete('/testimonies/delete', [TestimoniController::class, 'destroy'])->name('delete_testimoni');
     });
-    // Route::get('/billing', Billing::class)->name('billing');
-    // Route::get('/profile', Profile::class)->name('profile');
-    // Route::get('/tables', Tables::class)->name('tables');
-    // Route::get('/static-sign-in', StaticSignIn::class)->name('sign-in');
-    // Route::get('/static-sign-up', StaticSignUp::class)->name('static-sign-up');
-    // Route::get('/rtl', Rtl::class)->name('rtl');
-    // Route::get('/laravel-user-profile', UserProfile::class)->name('user-profile');
-    // Route::get('/laravel-user-management', UserManagement::class)->name('user-management');
 });
 
